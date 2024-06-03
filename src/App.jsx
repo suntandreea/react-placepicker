0fe309a7d121d22864d981e505e197e5208ef656
Andreea <suntandreea@protonmail.com>
2024-06-03 19:16:35 +0300

    optimized with useEffect & useCallback + geolocation & localStorage

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,43 @@
-import { useRef, useState } from 'react';
+import {useCallback, useEffect, useRef, useState} from 'react';
+import logoImg from './assets/logo.png';
+import DeleteConfirmation from './components/DeleteConfirmation.jsx';
+import Modal from './components/Modal.jsx';
 
 import Places from './components/Places.jsx';
-import { AVAILABLE_PLACES } from './data.js';
-import Modal from './components/Modal.jsx';
-import DeleteConfirmation from './components/DeleteConfirmation.jsx';
-import logoImg from './assets/logo.png';
+import {AVAILABLE_PLACES} from './data.js';
+import {sortPlacesByDistance} from './loc.js';
+
+const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+const storedPlaces = storedIds.map(id => AVAILABLE_PLACES.find(place => place.id === id));
+
 
 function App() {
-  const modal = useRef();
   const selectedPlace = useRef();
-  const [pickedPlaces, setPickedPlaces] = useState([]);
+  const [isOpen, setIsOpen] = useState(false);
+  const [pickedPlaces, setPickedPlaces] = useState(storedPlaces);
+  const [availablePlaces, setAvailablePlaces] = useState(AVAILABLE_PLACES);
+
+
+  useEffect(() => {
+    navigator.geolocation.getCurrentPosition(position => {
+      const sortedPlaces = sortPlacesByDistance(
+        AVAILABLE_PLACES,
+        position.coords.latitude,
+        position.coords.longitude
+      );
+      setAvailablePlaces(sortedPlaces);
+    });
+
+
+  }, []);
 
   function handleStartRemovePlace(id) {
-    modal.current.open();
+    setIsOpen(true);
     selectedPlace.current = id;
   }
 
   function handleStopRemovePlace() {
-    modal.current.close();
+    setIsOpen(false);
   }
 
   function handleSelectPlace(id) {
@@ -28,18 +48,26 @@ function App() {
       const place = AVAILABLE_PLACES.find((place) => place.id === id);
       return [place, ...prevPickedPlaces];
     });
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    if (!storedIds.includes(id)) {
+      localStorage.setItem('selectedPlaces', JSON.stringify([id, ...storedIds]));
+    }
   }
 
-  function handleRemovePlace() {
+  const handleRemovePlace = useCallback(function handleRemovePlace() {
     setPickedPlaces((prevPickedPlaces) =>
       prevPickedPlaces.filter((place) => place.id !== selectedPlace.current)
     );
-    modal.current.close();
-  }
+    setIsOpen(false);
+
+    const storedIds = JSON.parse(localStorage.getItem('selectedPlaces')) || [];
+    localStorage.setItem('selectedPlaces', JSON.stringify(storedIds.filter(id => id !== selectedPlace.current)));
+  }, []);
 
   return (
     <>
-      <Modal ref={modal}>
+      <Modal open={isOpen} onClose={handleStopRemovePlace}>
         <DeleteConfirmation
           onCancel={handleStopRemovePlace}
           onConfirm={handleRemovePlace}
@@ -63,7 +91,8 @@ function App() {
         />
         <Places
           title="Available Places"
-          places={AVAILABLE_PLACES}
+          places={availablePlaces}
+          fallbackText="Sorting places by distance..."
           onSelectPlace={handleSelectPlace}
         />
       </main>
--- src/components/DeleteConfirmation.jsx
+++ src/components/DeleteConfirmation.jsx
@@ -1,4 +1,22 @@
-export default function DeleteConfirmation({ onConfirm, onCancel }) {
+import {useEffect} from 'react';
+import ProgressBar from './ProgressBar.jsx';
+
+const INITIAL_TIMER = 3000;
+
+export default function DeleteConfirmation({onConfirm, onCancel}) {
+
+  useEffect(() => {
+    console.log('RUN EFFECT & SET TIMER');
+    const timer = setTimeout(() => {
+      onConfirm();
+    }, INITIAL_TIMER);
+
+    return () => {
+      console.log('CLEAR TIMER')
+      clearTimeout(timer);
+    }
+  }, [onConfirm]);
+
   return (
     <div id="delete-confirmation">
       <h2>Are you sure?</h2>
@@ -11,6 +29,7 @@ export default function DeleteConfirmation({ onConfirm, onCancel }) {
           Yes
         </button>
       </div>
+      <ProgressBar timer={INITIAL_TIMER} />
     </div>
   );
 }
